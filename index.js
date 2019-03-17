8424501e219cbb0429f1899b7a47a4d4c46733d6
Catherine Bronzo <cbronzo@mac.com>
2019-03-17 22:43:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ function BoardMember(name, homeState, training) {
   this.homeState = homeState
   this.training = training
   this.veto = function(){
+    console.log("No, I mus disagree")
   }
   this.approve = function(){
   }

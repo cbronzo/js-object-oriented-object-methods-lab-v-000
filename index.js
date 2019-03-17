7d95b9ea23ebe381dbd358fdd89d855e53f99ea3
Catherine Bronzo <cbronzo@mac.com>
2019-03-17 22:42:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,5 +2,11 @@ function BoardMember(name, homeState, training) {
   this.name = name
   this.homeState = homeState
   this.training = training
+  this.veto =
+  this.approve = 
+  this.doCharity = 
+  this.releasePressStatement = 
+  this.sayHi = 
+  
   
 }
